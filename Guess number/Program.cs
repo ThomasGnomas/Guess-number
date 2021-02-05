c51b7a811b70b7eae9cbd2989671373d30f6d4d5
ThomasGnomas <57919480+ThomasGnomas@users.noreply.github.com>
2021-02-05 11:20:17 +0100

    Add files via upload

---
+++ Guess number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userName = UserName();
+
+            Console.ReadKey();
+        }
+
+        static void ColorMessage(ConsoleColor color, string text){
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(text);
+
+            Console.ResetColor();
+        }
+
+        static string UserName(){
+            ColorMessage(ConsoleColor.Blue, "What's your name? ");
+
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+    }
+}
