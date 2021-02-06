f1070a0d612f60ad2c0aa336192108a3832acfcd
ThomasGnomas <57919480+ThomasGnomas@users.noreply.github.com>
2021-02-06 13:46:02 +0100

    Now user has 5 chances to guess number

--- Guess number/Program.cs
+++ Guess number/Program.cs
@@ -6,19 +6,35 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {   
+            Console.WriteLine("Welcome to number guesser! \nYou have 5 chances to guess the number from 1 to 100.");
+            
             Random random = new Random();
             int randomNumber = random.Next(1, 100);
-            
-            Console.Write("Your guess: ");
-            int userGuess = Convert.ToInt32(Console.ReadLine());
 
-            if (userGuess == randomNumber)
-            {
-                Console.WriteLine("Correct number");
-            }
-            else
-            {
-                Console.WriteLine("Wrong number");
+            for (int i = 1; i <=5; i++)
+            {   
+                if (i == 5)
+                {
+                    Console.Write($"\nIt's your last chance \n> ");
+                }
+                else
+                {
+                    Console.Write($"\nYour {i} chance \n> ");
+                }
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (userGuess == randomNumber)
+                {
+                Console.WriteLine("\nCorrect number");
+                }
+                else if (userGuess < randomNumber)
+                {
+                Console.WriteLine("Too small number");
+                }
+                else if (userGuess > randomNumber)
+                {
+                Console.WriteLine("Too big number");
+                }
             }
 
             Console.ReadKey();
