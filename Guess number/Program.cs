132add9f71495efba83c1b8149ddba2c9ab4df73
ThomasGnomas <57919480+ThomasGnomas@users.noreply.github.com>
2021-03-17 18:30:48 +0100

    Splited basic activitis into functions

--- Guess number/Program.cs
+++ Guess number/Program.cs
@@ -5,39 +5,55 @@ namespace C_sharp
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Welcome to number guesser! \nYou have 5 chances to guess the number from 1 to 100.");
+        {
+            Greeetings();
+            RandomNumer();
+
+            Console.ReadLine();
+        }
+
+        //Basic greetings
+        static void Greeetings()
+        {
+            Console.WriteLine("Welcome to number guesser! Choose the range of the random number.");
+        }
+
+        // taking first number from user
+        static int StRange()
+        {
+            Console.Write("st number: ");
+            int st = Convert.ToInt32(Console.ReadLine());
+
+            return st;
+        }
+
+        // taking second number from user
+        static int NdRange()
+        {
+            Console.Write("nd number: ");
+            int nd = Convert.ToInt32(Console.ReadLine());
+
+            return nd;
+        }
+
+        //converting both number into random number
+        static int RandomNumer()
+        {
+            int randomInt;
             
-            Random random = new Random();
-            int randomNumber = random.Next(1, 100);
+            int x = StRange();
+            int y = NdRange();
 
-            for (int i = 1; i <=5; i++)
-            {   
-                if (i == 5)
-                {
-                    Console.Write($"\nIt's your last chance \n> ");
-                }
-                else
-                {
-                    Console.Write($"\nYour {i} chance \n> ");
-                }
-                int userGuess = Convert.ToInt32(Console.ReadLine());
-
-                if (userGuess == randomNumber)
-                {
-                Console.WriteLine("\nCorrect number");
-                }
-                else if (userGuess < randomNumber)
-                {
-                Console.WriteLine("Too small number");
-                }
-                else if (userGuess > randomNumber)
-                {
-                Console.WriteLine("Too big number");
-                }
+            while (x > y)
+            {
+                Console.WriteLine($"First number is greater than second. Please retype first number (Second number is {y})");
+                x = StRange();
             }
+            
+            Random newRandom = new Random();
+            randomInt = newRandom.Next(x, y);
 
-            Console.ReadKey();
+            return randomInt;
         }
     }
 }
